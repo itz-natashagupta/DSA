330506d1552a38976222b4671304f821cfc3e4e0
Natasha Gupta <natashagupta55334@gmail.com>
2026-09-20 21:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (5.64%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        return nums[n/2];
+       
+        int candidate = 0;
+        int count = 0;
+        for(int num :  nums){
+            if(count == 0){
+                candidate = num;
+            }
+            if(num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
     }
 };
